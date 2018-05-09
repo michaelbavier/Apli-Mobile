8a7c243ac3a74d29478209775873c4fb00949324
michaelbavier <michaelbavier89@gmail.com>
2018-05-09 14:44:32 +0200

    update

--- src/index.js
+++ src/index.js
@@ -8,20 +8,26 @@ import arrows from './arrows.svg';
 
 ReactDOM.render(<MainContainer />, document.getElementById('root'));
 
+// MainCONTAINER
+
 
 function MainContainer(){
     return <div className="maincontainer"> <NoScroll/> <MessageForm/></div>
 }
 
-//MESSAGE
+// DIV NOSCROLL
 
 function NoScroll(){
     return <div className="noScroll"> <Container/></div>
 }
 
+// CONTAINER
+
 function Container(){
  return  <div className="container"> <Sender/> <Hour/> <Receiver/> <Hour/> <Sender/> <Hour/> <Receiver/> <Hour/> <Sender/> <Hour/> <Receiver/> <Hour/> <Sender/> <Hour/> <Receiver/> <Hour/><Sender/> <Hour/> <Receiver/> <Hour/> <Sender/> <Hour/> <Receiver/> <Hour/> <Sender/> <Hour/> <Receiver/> <Hour/> <Sender/> <Hour/> <Receiver/></div>
 }
+
+// BULLE DE MESSAGE
 
 function Sender(){
     return <div className="conversation sender"><p className="write">{"Oue a quel heure? a quel endrois? avec qui?"}</p></div>;
@@ -31,13 +37,14 @@ function Receiver(){
     return <div className="conversation receiver"><p className="write">{"hello les gars on boit un coup? oue avec Jean a Bruxelles vers 20h00  "}</p> </div>;
 }
 
+// L HEURE DES MESSAGES
+
 function Hour(){
     return <p className="hour">{"53 min ago"}</p>
 }
 
 
-// MESSAGE BOX
-
+// CHAT
 
 function MessageForm(){
     return  <div className="containerBox"><form className="messageForm"> <input className="text" /> <button className="arrows" ><i className="fa fa-arrow-up"></i></button> </form></div>
